e6894e202cc8203def3f4e107755eb8c586ad4d0
Sankalp Shankar <sankalps.23.beai@acharya.ac.in>
2026-03-03 14:40:17 +0530

    chore: Update Appwrite endpoint, project ID, and bucket ID.

--- frontend/src/appwrite.js
+++ frontend/src/appwrite.js
@@ -1,16 +1,16 @@
 import { Client, Storage, Functions } from 'appwrite';
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
-    .setProject('67bc876e0037a505bdeb'); // Replace with your Project ID
+    .setEndpoint('https://sgp.cloud.appwrite.io/v1') // Your Appwrite Endpoint
+    .setProject('69a67fa800077e349212'); // Replace with your Project ID
 
 const storage = new Storage(client);
 const functions = new Functions(client);
 
 // Note: Ensure you set up the correct Bucket ID and Project ID.
 const APPWRITE_CONFIG = {
-    projectId: '67bc876e0037a505bdeb', // Set your project ID here
-    bucketId: '67c570b5003c4078174f' // Set your bucket ID for payment proofs here
+    projectId: '69a67fa800077e349212', // Set your project ID here
+    bucketId: '69a692cd003ae3d94f50' // Set your bucket ID for payment proofs here
 };
 
 export { client, storage, functions, APPWRITE_CONFIG };
